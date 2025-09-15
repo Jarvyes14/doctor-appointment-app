1af8768b854d197bbab43eb2599af79c47954bf4
Jarvyes14 <javierbarcelosantos@gmail.com>
2025-09-15 17:16:11 -0600

    chore: admin route added

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::redirect('/', '/admin/');
+//Route::get('/', function () {
+//  return view('welcome');
+//});
 
 Route::middleware([
     'auth:sanctum',
